77f3ca93efe01159bb58ff000ec490393d48e19c
TRybina132 <tryb2516@gmail.com>
2022-12-31 18:46:57 +0200

    Remove safe areas

--- Tokens/MainPage.xaml.cs
+++ Tokens/MainPage.xaml.cs
@@ -6,4 +6,34 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
+    
+    protected override void OnAppearing()
+    {
+        // Remove safe areas
+#if IOS
+        
+        var scene = UIKit.UIApplication.SharedApplication.ConnectedScenes.ToArray<UIKit.UIWindowScene>().FirstOrDefault();
+        if (scene != null)
+        {
+            int factor = DeviceDisplay.Current.MainDisplayInfo.Orientation switch
+            {
+                DisplayOrientation.Portrait => 2,
+                DisplayOrientation.Landscape => 3,
+                _ => 2
+            };
+            
+            if (DeviceInfo.Current.Idiom == DeviceIdiom.Tablet)
+                factor = 2;
+
+            var windowScene = (UIKit.UIWindowScene)scene;
+            var safeArea = windowScene.Windows.FirstOrDefault()?.SafeAreaInsets;
+            Padding =  new Thickness(
+                -(safeArea.Value.Left), //left
+                Padding.Top, //safeArea.Value.Top, //-(safeArea.Value.Top * 2),// top, 
+                -(safeArea.Value.Right), //right
+                -(safeArea.Value.Bottom * factor)); //bottom
+        }
+#endif
+	
+    }
 }
