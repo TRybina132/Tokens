35e6fc2347b323cc8ceb6aa9aa69a75c6069b60c
TRybina132 <tryb2516@gmail.com>
2022-12-31 18:43:01 +0200

    add project

---
+++ Tokens/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Tokens;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ Tokens/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Tokens;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
